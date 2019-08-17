5a48b85f62414a9897c4f01ee59a53dd24a454bd
Sargnec <49839699+Sargnec@users.noreply.github.com>
2019-08-17 18:45:00 +0300

    Updates

---
+++ precache-manifest.24cd451c67b00839cf93c4fdb1c34d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4b002f8c3b1c308eb85ceec8ea29d0",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "2af4b0f22c9610e55f15",
+    "url": "/Todo-App/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "26d2eabcf727f50ad98d",
+    "url": "/Todo-App/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2af4b0f22c9610e55f15",
+    "url": "/Todo-App/static/js/2.f9dc8f41.chunk.js"
+  },
+  {
+    "revision": "26d2eabcf727f50ad98d",
+    "url": "/Todo-App/static/js/main.d969c03b.chunk.js"
+  },
+  {
+    "revision": "8d194b33d19f613dfabb",
+    "url": "/Todo-App/static/js/runtime~main.d3cc1a11.js"
+  }
+]);
